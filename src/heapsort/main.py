e88331d1bf9bf06d1272793875ca3782de785615
luizfernando <luiz-fsilva37@hotmail.com>
2019-11-17 23:32:45 -0300

    Adição HeapSort

---
+++ src/heapsort/main.py
@@ -0,0 +1,23 @@
+import random
+from algoritmo import heapSort
+
+def criar_lista(tamanho, ordenacao):
+    numeros = [random.randrange(tamanho) for x in range(tamanho)]
+    if ordenacao == 'D':
+        return sorted(numeros, reverse=True)
+    elif ordenacao == 'C':
+        return sorted(numeros)
+    elif ordenacao == 'A':
+        return numeros
+
+tamanho = 1000
+ordenacao = "D"
+
+lista = criar_lista(tamanho, ordenacao)
+print("Lista gerada: ")
+print(lista)
+
+lista = [int(x) for x in lista]
+heapSort(lista)
+print("Lista ordenada: ")
+print(lista)
